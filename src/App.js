11cf43d6475a8a32c82378c130be592d22db33fe
min <earnestga@naver.com>
2023-01-18 19:37:46 +0900

    반복문 사용없이, 좋아요수 개별적 증가처리

--- src/App.js
+++ src/App.js
@@ -6,42 +6,66 @@ import {useState} from 'react';
 function App() {
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
-  let [좋아요, 좋아요변경] = useState(0);
+  let [좋아요, 좋아요변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
+  
 
   return (
     <div className="App">
       <div className="black-nav"> 
         <h4>ReactBlog</h4>
       </div>
-
-      <button onClick = { () => {
-        let copy = [...글제목];
-        copy[0] = '여자코트 추천';
-        글제목변경(copy);
-      }}> 수정버튼 </button>
       
-      <button onClick={ () => {
+      <button className='arrange-btn' onClick={ () => {
         let copy2 = [...글제목];
         copy2.sort();
         글제목변경(copy2);
       }}>가나다순정렬</button>
 
       <div className="list">
-        <h4>{글제목[0]} <span onClick={ () => {좋아요변경(좋아요+1)} }>👍</span> {좋아요} </h4>
+        <h4>{글제목[0]} <span onClick={
+           () => {
+            let good = [...좋아요];
+            good[0] = good[0] + 1;
+            좋아요변경(good)
+            } }>👍</span> {좋아요[0]} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[1]}</h4>
+        <h4>{글제목[1]}<span onClick={ () => {
+          let good2 = [...좋아요];
+          good2[1] = good2[1] + 1;
+          좋아요변경(good2)} }>👍</span> {좋아요[1]} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4>{글제목[2]}<span onClick={ () => {
+          let good3 = [...좋아요];
+          good3[2] = good3[2] + 1;
+          좋아요변경(good3)} }>👍</span> {좋아요[2]} </h4>
         <p  style={{display:'inline-block', marginRight: '10px'}}>2월 17일 발행</p>
         <button onClick={ ()=> {
         modal == false ? setModal(true) : setModal(false)
         }}>모달버튼</button>
       </div>
+
+      {/* {
+        글제목.map(function(a,i){
+          return (
+            <div className="list" key={i}>
+              <h4 onClick = { () => {setModal(!modal)}} > 
+                {글제목[i]}
+                  <span onClick={ () => {
+                   좋아요변경(좋아요[i]+1);
+                  } }>👍</span> 
+               {좋아요[i]}
+              </h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      } */}
+
 
       {
         modal == true ? <Modal/> : null
